a2456a4d58912e0b5f47010ace7c062e7d3eca69
jjf21 <carl.quivron@gmail.com>
2021-02-04 15:12:43 +0100

    add covid methods to user

--- app/models/injection.rb
+++ app/models/injection.rb
@@ -1,3 +1,8 @@
 class Injection < ApplicationRecord
   belongs_to :user
+
+  def self.duration_btw_injections(injection_1, injection_2)
+    difference = (injection_1.date.to_datetime - injection_2.date.to_datetime).to_f
+    (difference / 365 * 12).round
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,47 @@ class User < ApplicationRecord
   validates :birth_date, presence: true
 
   def up_to_date_with_covid?
+    return false unless injections.any?
+
+    case age_in_days
+    when 0...14
+      check_covid_under_14
+    when 14..65
+      check_covid_btw_14_65
+    else
+      check_covid_over_65
+    end
+  end
+
+  def check_covid_under_14
+    return false unless injections.count >= 2
+
+    duration_btw_injections = []
+    injections.each do |inj_1|
+      injections.each do |inj_2|
+        duration_btw_injections << Injection.duration_btw_injections(inj_1, inj_2)
+      end
+    end
+    duration_btw_injections.uniq.reject{|duration| duration < 6}.any?
+  end
+
+  def check_covid_btw_14_65
+    injections.each do |injection|
+      return true if injection.date > 36.months.ago
+    end
+
+    false
+  end
+
+  def check_covid_over_65
+    injections.each do |injection|
+      return true if injection.date > 12.months.ago
+    end
+
+    false
+  end
+
+  def age_in_days
+    ((Time.zone.now - birth_date.to_time) / 1.year.seconds).floor
   end
 end
